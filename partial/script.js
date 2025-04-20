4b0c9a9f4d5dc892b675bcbe9498d007c24ffd8c
AlisonLS1 <alisonsales419@gmail.com>
2025-04-20 14:19:42 -0300

    hello world

---
+++ partial/script.js
@@ -0,0 +1,8 @@
+let youtube = document.getElementById("youtube")
+youtube.addEventListener("mouseover", ()=>{
+    youtube.classList.add("fa-fade")
+})
+youtube.addEventListener("mouseout", ()=>{
+    youtube.classList.remove("fa-fade")
+})
+
